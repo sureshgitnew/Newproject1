b21eebc638b6601bff2f14f5f51e29bf288b8a2d
sureshgitnew <53163973+sureshgitnew@users.noreply.github.com>
2024-08-26 19:00:36 +0530

    Create Hello.java

---
+++ Hello.java
@@ -0,0 +1,12 @@
+public class Hello {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello, World!");
+
+    }
+
+}
+
+##for execution javac Hello.java & java Hello
+
