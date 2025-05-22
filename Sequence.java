1e5f4b3d6f0fb28286ca3aa34e33120e4d6e1451
sureshgitnew <53163973+sureshgitnew@users.noreply.github.com>
2025-05-22 08:02:10 +0530

    Create Sequence.java

---
+++ Sequence.java
@@ -0,0 +1,9 @@
+public class Sequence {
+    public static void main(String[] args) {
+        // Print numbers in sequence: 1 2 3 4 5 6
+        for (int i = 1; i <= 6; i++) {
+            System.out.print(i + " ");
+        }
+        System.out.println(); // Move to next line after printing
+    }
+}
